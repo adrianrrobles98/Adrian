902c9daa242e18bca4d3d5c9fb173a8362a85461
adria <adria@LAPTOP-P7EG80S0>
2019-01-10 21:00:39 +0100

    Versión 0.0

---
+++ src/arrays.java
@@ -0,0 +1,15 @@
+
+public class arrays {
+
+	public static void main(String[] args) {
+		
+		
+		// int[]mi_matriz=new int [10] para declarar el array
+		
+		int[]mi_matriz={2,4,6,8,10,};
+		
+		
+		
+	}
+
+}
